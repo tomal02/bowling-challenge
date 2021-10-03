6df400a816dfb5c9c449851ed47479f0f02491b4
Tom Lee <tomalee2002@gmail.com>
2021-10-03 21:01:03 +0100

    adds full spec for returning the full score (including bonuses)

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,5 +1,20 @@
 /* eslint-env jasmine */
 
 describe('Game', function () {
-  it('can return all the scores and whether they were strikes or spares', function () {});
+  it('can return all the scores with the correct bonus added on top', function () {
+    testGame = new Game();
+    testGame.enterScores(
+      [6, 0],
+      [6, 0],
+      [10, null],
+      [2, 0],
+      [6, 1],
+      [4, 1],
+      [4, 1],
+      [9, 1],
+      [6, 1],
+      [6, 1]
+    );
+    expect(testGame.scores()).toBe([6, 6, 12, 2, 7, 5, 5, 16, 7, 7]);
+  });
 });
