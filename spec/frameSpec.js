0f5cb18c2560dbc81ac701874e417442008226aa
Tom Lee <tomalee2002@gmail.com>
2021-10-03 13:58:04 +0100

    adds spec test for checking for strike or spare

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,4 +9,19 @@ describe("Frame", function () {
     testFrame.rolls(10, null);
     expect(testFrame.total()).toBe(10);
   });
+  it("can return that a frame contains a strike", function () {
+    testFrame = new Frame();
+    testFrame.rolls(10, 0);
+    expect(testFrame.strikeOrSpare).toBe("Strike");
+  });
+  it("can return that a frame contains a spare", function () {
+    testFrame = new Frame();
+    testFrame.rolls(6, 4);
+    expect(testFrame.strikeOrSpare).toBe("Spare");
+  });
+  it("can return null when frame contains neither a strike or spare", function () {
+    testFrame = new Frame();
+    testFrame.rolls(4, 5);
+    expect(testFrame.strikeOrSpare).toBe(null);
+  });
 });
