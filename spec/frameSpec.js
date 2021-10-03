ee8378de11e1c8485c6490929b7efb5575a7e41b
Tom Lee <tomalee2002@gmail.com>
2021-10-03 13:39:48 +0100

    adds ability to store pins knocked for each roll and total()

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,14 +1,12 @@
 describe("Frame", function () {
   it("can store the pins knocked for each roll", function () {
     testFrame = new Frame();
-    testFrame.rollOne(9);
-    testFrame.rollTwo(0);
-    expect(testFrame.pinsKnocked).toBe([9, 0]);
+    testFrame.rolls(9, 0);
+    expect(testFrame.pinsKnocked).toEqual([9, 0]);
   });
   it("can return the total pins knocked for both rolls", function () {
     testFrame = new Frame();
-    testFrame.rollOne(6);
-    testFrame.rollTwo(4);
+    testFrame.rolls(10, null);
     expect(testFrame.total()).toBe(10);
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -3,5 +3,16 @@
 class Frame {
   constructor() {
     const maximumTotal = 10;
+    this.pinsKnocked = [];
+  }
+
+  rolls(rollOne, rollTwo) {
+    this.pinsKnocked = [rollOne, rollTwo];
+  }
+
+  total() {
+    let total = 0;
+    this.pinsKnocked.forEach((pins) => (total += pins));
+    return total;
   }
 }
